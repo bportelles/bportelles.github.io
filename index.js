1cac0f302703277a22c299631c9de1acc92478f0
Bryan Portelles <bryanportelles27@gmail.com>
2024-03-06 01:24:24 -0500

    Changes to be committed: 	modified:   .gitignore 	new file:   computer.png 	new file:   favicon.ico 	modified:   index.html 	modified:   index.js 	modified:   style.css 	new file:   terminal.gif
    - Added new functions, css parameters, div enclosures
    - Added new gif on side
    - Added new "slide bar" showing your position on the site
    - Added little icons and QOL things
    - Fixed issue with cursor
    - Fixed issue with text resizing

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ $(document).ready(function () {
 
     $("#aboutme").mouseenter(function () {
         $(this).animate({
-            boxShadow: "5px 0px 15px rgba(0, 0, 0, 0.3)",
+            boxShadow: "5px 5px 15px rgba(0, 0, 0, 0.3)",
         }, 300);
+
     });
     
     $("#aboutme").mouseleave(function () {
@@ -27,35 +28,83 @@ $(document).ready(function () {
         }, 300);
     });
 
+    $(window).scroll(function() {
+        let scrollPosition = $(this).scrollTop();
+
+        pageIndicator = $("#pagePositionIndicator");
+
+        console.log($("#schoolingHeaderWithIcon").offset.top);
+
+        let ABOUT_ME_POS = (0 <= scrollPosition) && (scrollPosition < $("#schoolingHeaderWithIcon").position().top);
+        let CS_DEGREE_POS = ($("#schoolingHeaderWithIcon").position().top <= scrollPosition) && (scrollPosition < $("#projectsHeaderWithIcon").position().top);
+        let PROJECT_POS = ($("#projectsHeaderWithIcon").position().top <= scrollPosition) && (scrollPosition < $("#experienceHeaderWithIcon").position().top);
+        let WORK_EXPERIENCE_POS = ($("#experienceHeaderWithIcon").position().top <= scrollPosition) && (scrollPosition < $("#skillsHeaderWithIcon").position().top);
+        let SKILLS_TECHNOLOGIES_POS = ($("#skillsHeaderWithIcon").position().top <= scrollPosition) && (scrollPosition < 2629);
+
+        let aboutMeButton = $("#scrollToAboutMe");
+        let degreeButton = $("#scrollToDegree");
+        let projectsButton = $("#scrollToProjects");
+        let workButton = $("#scrollToWork");
+        let skillsButton = $("#scrollToSkills");
+
+        // Originally had written this as a switch, did not think
+        // this logic through for non awkard parameter, still works in every scenario
+
+        switch (pageIndicator != -1) {
+            case (ABOUT_ME_POS):
+                console.log("ABOUT ME TRUE");
+                console.log(aboutMeButton.position().top);
+                pageIndicator.offset({top: (aboutMeButton.position().top + scrollPosition)});
+                break;
+            case (CS_DEGREE_POS):
+                console.log("CS POS TRUE");
+                pageIndicator.offset({top: (degreeButton.position().top + scrollPosition)});
+                break;
+            case (PROJECT_POS):
+                console.log("PROJECT POS TRUE");
+                pageIndicator.offset({top: (projectsButton.position().top + scrollPosition)});
+                break;
+            case (WORK_EXPERIENCE_POS):
+                console.log("WORK POS TRUE");
+                pageIndicator.offset({top: (workButton.position().top + scrollPosition)});
+                break;
+            case (SKILLS_TECHNOLOGIES_POS):
+                console.log("SKILLS POS TRUE");
+                pageIndicator.offset({top: (skillsButton.position().top + scrollPosition)});
+                break;
+        }
+        //console.log(scrollPosition);
+    });
+
     $("#scrollToAboutMe").click(function () {
         $("html, body").animate({
-            scrollTop: $("#aboutme").offset().top
+            scrollTop: ($("#aboutme").offset().top) - 100
         }, 300);
     });
     $("#scrollToProjects").click(function () {
         $("html, body").animate({
-            scrollTop: $("#projects").offset().top
-        }, 100);
+            scrollTop: ($("#projects").offset().top) - 100
+        }, 300);
     });
     $("#scrollToSkills").click(function () {
         $("html, body").animate({
-            scrollTop: $("#skills").offset().top
-        }, 100);
+            scrollTop: ($("#skills").offset().top) - 100
+        }, 300);
     });
     $("#scrollToWork").click(function () {
         $("html, body").animate({
-            scrollTop: $("#experience").offset().top
-        }, 100);
+            scrollTop: ($("#experience").offset().top) - 100
+        }, 300);
     });
     $("#scrollToDegree").click(function () {
         $("html, body").animate({
-            scrollTop: $("#schooling").offset().top
-        }, 100);
+            scrollTop: ($("#schooling").offset().top) - 100
+        }, 300);
     });
 
     function repeatBlinkerFadeAnim() {
         $("#blinker").animate({opacity: 0}, 300, function () {
-            $(this).animate({opacity: 1}, 1400, repeatBlinkerFadeAnim);
+            $(this).animate({opacity: 1}, 1500, repeatBlinkerFadeAnim);
         });
     }
 
